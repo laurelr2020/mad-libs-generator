44ef4ba2309a738b2b1da26890093f334ca750c1
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-22 19:46:40 -0400

    intial set up.

---
+++ mad-libs.py
@@ -0,0 +1,17 @@
+
+def generateMadLib():
+    print("*******  Welcome  **********")
+
+
+answer = input("Would you like to do a Mad Lib? (yes/no) ")
+
+if answer == "yes":
+    generateMadLib()
+elif answer == "no":
+    print("okay...byee")
+else:
+    print("not a valid input")
+
+
+
+
