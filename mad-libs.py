84805fdc8267f5ef90cf419e64c5bf55a021c370
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-22 21:38:12 -0400

    added story line with filled in values.

--- mad-libs.py
+++ mad-libs.py
@@ -22,13 +22,24 @@ def generateMadLib():
     adjective = input("Adjective: ")
     adverb = input("Adverb: ")
     sillyName3 = input("Third Silly Name: ")
-    sillyName4 = input("One More Silly Name: ")
+    sillyName4 = input("Fourth Silly Name: ")
     facialFeature = input("Facial Feature: ")
+    city = input("US City: ")
+    sillyName5 = input("Fifth Silly Name: ")
     verb = input("Verb: ")
     noun = input("Noun: ")
     actor = input("Actor: ")
     noun2 = input("Noun: ")
 
-    madLibList = [sillyName, unrealisticProfession, country, sillyName2, color, adjective, adverb, sillyName3, sillyName4, facialFeature, verb, noun, actor, noun2]
+    madLibList = [sillyName, unrealisticProfession, country, sillyName2, color, adjective, adverb, sillyName3, sillyName4, facialFeature, city, sillyName5, verb, noun, actor, noun2]
+    populateMadLib(madLibList)
+
+def populateMadLib(inputs):
+    print("Meet our hero " + inputs[0] + ", a super intelligent " + inputs[1] + ".")
+    print("A run-in with the military of" + inputs[2] + " leads him to create his alter-ego, " + inputs[3] + ", a " + " " + inputs[4] + " " + inputs[5] + " giant caplable of great destuction.")
+    print("He " + inputs[6] + " battles the military with his girlfriend " + inputs[7] + ".")
+    print("Eventually it is discovered that our hero's long-time colleague, " + inputs[8] + ", distinguished by his " + inputs[9] + ", is trying to turn " + inputs[3] + " into a weapon, leading to a climatic (if pointless) battle in downtown " + inputs[10] + "  with an evil version of the same giant alter-ego called " + inputs[11] + ".")
+    print("Eventually the enemy is subdued by " + inputs[12] + "ing him with a " + inputs[13] + ".")
+    print("In the final reel, " + inputs[14] + " appears to propose joining him in a " + inputs[15] + ".")
 
 promptBeginning()
