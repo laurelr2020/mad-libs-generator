05d35b03979febf6e75a9c3bf6ccd7dac4bc669b
laurelr2020 <laurelr2020@mountunion.edu>
2019-08-24 15:02:17 -0400

    fixed spelling errors .

--- mad-libs.py
+++ mad-libs.py
@@ -26,7 +26,7 @@ def generateMadLib():
     facialFeature = input("Facial Feature: ")
     city = input("US City: ")
     sillyName5 = input("Fifth Silly Name: ")
-    verb = input("Verb: ")
+    verb = input("Verb ending in -ing: ")
     noun = input("Noun: ")
     actor = input("Actor: ")
     noun2 = input("Noun: ")
@@ -36,10 +36,10 @@ def generateMadLib():
 
 def populateMadLib(inputs):
     print("Meet our hero " + inputs[0] + ", a super intelligent " + inputs[1] + ".")
-    print("A run-in with the military of" + inputs[2] + " leads him to create his alter-ego, " + inputs[3] + ", a " + " " + inputs[4] + " " + inputs[5] + " giant caplable of great destuction.")
+    print("A run-in with the military of " + inputs[2] + " leads him to create his alter-ego, " + inputs[3] + ", a " + " " + inputs[4] + " " + inputs[5] + " giant, capable of great destruction.")
     print("He " + inputs[6] + " battles the military with his girlfriend " + inputs[7] + ".")
     print("Eventually it is discovered that our hero's long-time colleague, " + inputs[8] + ", distinguished by his " + inputs[9] + ", is trying to turn " + inputs[3] + " into a weapon, leading to a climatic (if pointless) battle in downtown " + inputs[10] + "  with an evil version of the same giant alter-ego called " + inputs[11] + ".")
-    print("Eventually the enemy is subdued by " + inputs[12] + "ing him with a " + inputs[13] + ".")
-    print("In the final reel, " + inputs[14] + " appears to propose joining him in a " + inputs[15] + ".")
+    print("Eventually the enemy is subdued by " + inputs[12] + " him with a " + inputs[13] + ".")
+    print("In the final reel, " + inputs[14] + " joins him in a " + inputs[15] + ".")
 
 promptBeginning()
